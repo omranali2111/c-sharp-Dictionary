02a15107f11c0cc2d6ee60313e3433b863958484
omranali2111 <aljabri1234@hotmail.com>
2023-07-13 12:21:55 +0400

    csharp dictionary

---
+++ c-sharp-Dictionery/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_Dictionery
+{
+    internal class Person
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person(long id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return ($"name:{name} age:{age}");
+        }
+
+       
+    }
+    
+}
---
+++ c-sharp-Dictionery/Program.cs
@@ -0,0 +1,57 @@
+﻿using c_sharp_Dictionery;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1=new Person(1222,"omran",27);
+        Person person2 = new Person(1223, "ali", 22);
+        Person person3 = new Person(1225, "salim", 30);
+
+        Dictionary<long,Person> personMap = new Dictionary<long,Person>();
+
+        personMap.Add(person1.id, person1);
+        personMap.Add(person2.id, person2);
+        personMap.Add(person3.id, person3);
+
+       Console.WriteLine(personMap[person1.id]);
+
+        person1.age = 50;
+
+        Person? result=null;
+        if(personMap.TryGetValue(person1.id, out result))
+        {
+            Console.WriteLine(result);
+        }
+
+        Console.WriteLine("Enter the ID number to retrieve person information:");
+        string input = Console.ReadLine();
+        long id;
+
+        if (long.TryParse(input, out id))
+        {
+            GetPersonInfo(id, personMap);
+        }
+        else
+        {
+            Console.WriteLine("Invalid ID number entered.");
+        }
+
+        Console.ReadLine();
+
+    }
+    static void GetPersonInfo(long id, Dictionary<long, Person> personMap)
+    {
+        if (personMap.ContainsKey(id))
+        {
+            Person person = personMap[id];
+            Console.WriteLine($"ID: {person.id}, Name: {person.name}, Age: {person.age}");
+        }
+        else
+        {
+            Console.WriteLine("Person with the specified ID does not exist.");
+        }
+    }
+
+
+}
